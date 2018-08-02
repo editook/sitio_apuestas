baf8cb37d816064271aac9f37e230c6d648c6b3d
eduardo perez <edito.ok@gmail.com>
2018-08-02 19:48:33 -0400

    panel y distribucion de plantilla

---
+++ pagina_apuestas/resources/views/Panel/Panel_central_inicio.blade.php
@@ -0,0 +1,97 @@
+
+<div class="container">
+	        <div class="row">
+		        <div class="col-sm-11 col-sm-offset-1">
+		            <!--      Wizard container        -->
+		            <div class="wizard-container">
+		                <div class="card wizard-card" data-color="purple" id="wizard">
+			                <form action="" method="">
+			                <!--        You can switch " data-color="rose" "  with one of the next bright colors: "blue", "green", "orange", "purple"        -->
+
+		                    	<div class="wizard-header">
+		                        	<h3 class="wizard-title">
+		                        		List Your Place
+		                        	</h3>
+									<h5>This information will let us know more about your place.</h5>
+		                    	</div>
+								<div class="wizard-navigation">
+									<ul>
+			                            <li><a href="#location" data-toggle="tab">Location</a></li>
+			                            <li><a href="#type" data-toggle="tab">Type</a></li>
+			                            <li><a href="#facilities" data-toggle="tab">Facilities</a></li>
+			                            <li><a href="#description" data-toggle="tab">Description</a></li>
+			                        </ul>
+								</div>
+
+		                        <div class="tab-content">
+		                            <div class="tab-pane" id="location">
+		                            	<div class="row">
+		                                	<div class="col-sm-12">
+		                                    	<h4 class="info-text"> Let's start with the basic details</h4>
+		                                	</div>
+		                                	<div class="col-sm-5 col-sm-offset-1">
+		                                    	
+		                                	</div>
+		                                	<div class="col-sm-5">
+		                                    	
+		                                	</div>
+		                                	<div class="col-sm-5 col-sm-offset-1">
+		                                    	
+		                                	</div>
+		                                	<div class="col-sm-5">
+		                                    	
+		                                	</div>
+		                            	</div>
+		                            </div>
+		                            <div class="tab-pane" id="type">
+		                                <h4 class="info-text">What type of location do you have? </h4>
+		                                <div class="row">
+		                                    <div class="col-sm-10 col-sm-offset-1">
+		                                        <div class="col-sm-4 col-sm-offset-2">
+		                                            
+		                                        </div>
+		                                        <div class="col-sm-4">
+		                                            
+		                                        </div>
+		                                    </div>
+		                                </div>
+		                            </div>
+		                            <div class="tab-pane" id="facilities">
+		                                <h4 class="info-text">Tell us more about facilities. </h4>
+		                                <div class="row">
+		                                    <div class="col-sm-5 col-sm-offset-1">
+		                                      
+		                                    </div>
+		                                    <div class="col-sm-5">
+		                                    	
+		                                    </div>
+		                                    <div class="col-sm-5 col-sm-offset-1">
+		                                    	
+		                                    </div>
+		                                    <div class="col-sm-5">
+		                                    	
+	                                    	</div>
+		                                </div>
+		                            </div>
+		                            <div class="tab-pane" id="description">
+		                                <div class="row">
+		                                    <h4 class="info-text"> Drop us a small description. </h4>
+		                                    <div class="col-sm-6 col-sm-offset-1">
+		                                        
+		                                    </div>
+		                                    <div class="col-sm-4">
+		                                    	
+		                                    </div>
+		                                </div>
+		                            </div>
+		                        </div>
+		                        <div class="wizard-footer">
+	                            	
+		                            <div class="clearfix"></div>
+		                        </div>
+			                </form>
+		                </div>
+		            </div> <!-- wizard container -->
+		        </div>
+	        </div> <!-- row -->
+</div> <!--  big container -->
--- pagina_apuestas/resources/views/Plantilla_inicio/Base.blade.php
+++ pagina_apuestas/resources/views/Plantilla_inicio/Base.blade.php
@@ -1,23 +1,38 @@
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="static/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('icon/favicon.ico') }}"/>
+    <link rel="icon" type="image/png" sizes="96x96"  href="{{ asset('icon/favicon.png') }}"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Light Bootstrap Dashboard Vue</title>
-    <!-- Bootstrap core CSS     -->
+    <title>Bets</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <!--     Fonts and icons     -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="static/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="{{ asset('csss/bootstrap.css')}}" rel="stylesheet" />
+    <link href='https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet"  href="{{ asset('csss/fonts/pe-icon-7-stroke.css') }}"/>
 
+    <!--link rel="stylesheet" href="{{ asset('csss/bootstrap.min.css') }}"-->
+    <link rel="stylesheet" href="{{ asset('csss/material-bootstrap-wizard.css') }}">
+    <!--link rel="stylesheet" href="{{ asset('csss/demo.css') }}"-->
+    <link rel="stylesheet"  href="{{ asset('csss/gaia.css') }}">
+    @yield('estilos_nuevos')
   </head>
   <body>
-    <div id="app"></div>
-    <!-- built files will be auto injected -->
+      @include('Panel.Panel_lateral_superior')
+      @include('Panel.Panel_central_inicio')
+
   </body>
+  <!--script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script-->
+  <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/modernizr.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/gaia.js')}}"></script>
+
+
+  <script type="text/javascript" src="{{asset('js/jquery-2.2.4.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.bootstrap.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/material-bootstrap-wizard.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.validate.min.js')}}"></script>
+  @yield('scripts_nuevos')
 </html>
